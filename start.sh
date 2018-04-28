d095b53ba0daf3ca8c517498b0f45705388b5dcd
yiwang6 <admin@yiwang6.cn>
2018-04-28 19:47:36 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+echo >"/www/wdlinux/mysql-5.5.58/data/`hostname`.pid" 
 service wdcp start
 service httpd start
+service mysqld start
 service nginxd start
 service pureftpd start
 tail -f /dev/null
